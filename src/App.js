ff8c353df57247df6e0a947d08e5425e97a3b0d7
Ankit Tanna <A.Tanna@riotinto.com>
2024-07-26 13:12:16 +1000

    pass props to left and right components

--- src/App.js
+++ src/App.js
@@ -2,20 +2,20 @@ import logo from "./logo.svg";
 import "./App.css";
 import { SplitScreen } from "./SplitScreen";
 
-const LeftHandComponent = () => {
-  return <h1 style={{ backgroundColor: "green" }}>Left!</h1>;
+const LeftHandComponent = ({name}) => {
+  return <h1 style={{ backgroundColor: "green" }}>Left! {name}</h1>;
 };
 
-const RightHandComponent = () => {
-  return <h1 style={{ backgroundColor: "red" }}>Right!</h1>;
+const RightHandComponent = ({message}) => {
+  return <h1 style={{ backgroundColor: "red" }}>Right! {message}</h1>;
 };
 
 function App() {
   return (
     <>
       <SplitScreen leftWeight={1} rightWeight={3}>
-        <LeftHandComponent />
-        <RightHandComponent />
+        <LeftHandComponent name="Ankit"/>
+        <RightHandComponent message="Hello"/>
       </SplitScreen>
     </>
   );
